2b6f8e736a2e4f2523029fc38df5ef49d3b61e36
Geri Angela Paragas <gparagas@up.edu.ph>
2022-05-13 02:53:32 +0800

    Slightly prettified toolbar, added mild error handling

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ from kivy.properties import ObjectProperty, StringProperty
 from kivy.uix.widget import Widget 
 from kivy.base import runTouchApp
 from kivy.lang import Builder
+from kivy.core.window import Window
+
 
 # from tkinter import *
 from tkinter import Tk
@@ -72,23 +74,29 @@ class MyGridLayout(Widget):
     # FILE OPERATIONS   ===============================================
 
     def load(self, path, filename):
-        with open(os.path.join(path, filename[0])) as stream:
-            self.ids.text_input.text = stream.read()
-        self.popup.dismiss()
+        try:
+            with open(os.path.join(path, filename[0])) as stream:
+                self.ids.text_input.text = stream.read()
+            self.popup.dismiss()
+        except:
+            print("[ERROR] Load file failed")
     
 
     def open(self):
         Tk().withdraw()
-        path = filedialog.askopenfilename()
-        self.file_path = path
-        if path:
-            global current
-            current = path
-            self.ids.file_open.text = "Editing: " + path #shows what file is currently open
-            file = open(path, 'r')
-            contents = file.read()
-            file.close()
-            self.ids.text_input.text = contents
+        try:
+            path = filedialog.askopenfilename()
+            self.file_path = path
+            if path:
+                global current
+                current = path
+                self.ids.file_open.text = "Editing: " + path #shows what file is currently open
+                file = open(path, 'r')
+                contents = file.read()
+                file.close()
+                self.ids.text_input.text = contents
+        except:
+            print("[ERROR] Open file failed")
 
 
     def delete(self):
@@ -96,22 +104,28 @@ class MyGridLayout(Widget):
         self.ids.text_input.text = ""
 
         global current
-        if os.path.isfile(current):
-            os.remove(current)
-            messagebox.showinfo("Delete", "File successfully deleted.")
-            self.ids.file_open.text = "No Open File" 
+        try:
+            if os.path.isfile(current):
+                os.remove(current)
+                messagebox.showinfo("Delete", "File successfully deleted.")
+                self.ids.file_open.text = "No Open File" 
+        except:
+            print("[ERROR] Delete file failed")
 
 
     def saveAs(self):
         Tk().withdraw() # hide tkinter window
-        path = filedialog.asksaveasfilename(defaultextension=".txt",filetypes=[("All Files","*.*"),("Text Documents","*.txt")])
-        if path:
-            file = open(path, 'w')
-            file.write(self.ids.text_input.text)
-            messagebox.showinfo("Save As", "File successfully saved.")
+        try:
+            path = filedialog.asksaveasfilename(defaultextension=".txt",filetypes=[("All Files","*.*"),("Text Documents","*.txt")])
+            if path:
+                file = open(path, 'w')
+                file.write(self.ids.text_input.text)
+                messagebox.showinfo("Save As", "File successfully saved.")
 
-        global current
-        current = path
+            global current
+            current = path
+        except:
+            print("[ERROR] Save As file failed")
 
 
     def new(self):
@@ -131,10 +145,13 @@ class MyGridLayout(Widget):
         # checks whether a previously saved file is currently open
         global current
         if current:
-            file = open(current, 'w')
-            file.write(self.ids.text_input.text)
-            file.close()
-            messagebox.showinfo("Save", "File successfully saved.")
+            try:
+                file = open(current, 'w')
+                file.write(self.ids.text_input.text)
+                file.close()
+                messagebox.showinfo("Save", "File successfully saved.")
+            except:
+                print("[ERROR] Save file failed")
         else:
             self.saveAs()
     
@@ -144,11 +161,14 @@ class MyGridLayout(Widget):
         global current
         if current:
             moveHere = filedialog.askdirectory()
-            shutil.move(current, moveHere)
-            current = False
-            self.ids.file_open.text = "No Open File"
-            self.ids.text_input.text = ""
-            messagebox.showinfo("Move", "File successfully moved.")
+            try:
+                shutil.move(current, moveHere)
+                current = False
+                self.ids.file_open.text = "No Open File"
+                self.ids.text_input.text = ""
+                messagebox.showinfo("Move", "File successfully moved.")
+            except:
+                print("[ERROR] Move file failed")
         else:
             messagebox.showinfo("Move", "Please open a file and try again.")
 
@@ -188,29 +208,14 @@ class MyGridLayout(Widget):
                 self.ids.search_matches.add_widget(result)
 
 
-    # ERROR INTERFACE   ===============================================
-
-    # terminal interface to manually select what error to trigger
-    def error_trigger(self):
-        
-        errors = ["Read-only Error", "Create File Failure"]
-        print("Please select what error you wish to trigger:")
-
-        for i in range(len(errors)):
-            print('\t{0} {1}'.format(str(i), errors[i]))
-
-        whichError = int(input("Enter the index of the error: "))
-        
-        if whichError in range(len(errors)):
-            print("ERROR: {0}.\n".format(errors[whichError]))
-        
-
-
 class MyApp(App):
 
     def build(self):
         global layouts
         layouts = MyGridLayout()
+
+        self.title = "Loom Notetaker"
+
         return layouts
 
 
